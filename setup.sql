812e3d15f09cec30f5c0400b69f54cc0ee94a934
Valentine Nyibiam <saanmoyolnyibiam@gmail.com>
2024-08-31 15:05:01 +0000

    cleanup(root): rename setupfile

--- setup.sql
+++
@@ -1,8 +0,0 @@
-DROP USER IF EXISTS 'vw_user'@'localhost';
-CREATE USER IF NOT EXISTS
-              'vw_user'@'localhost'
-              IDENTIFIED BY 'vw_user#92';
-CREATE DATABASE IF NOT EXISTS votewave_db;
-GRANT ALL PRIVILEGES ON votewave_db.* TO 'vw_user'@'localhost';
-FLUSH PRIVILEGES;
-
