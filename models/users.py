9259cf62feca348484f79880cd373bc07556fa8e
Valentine Nyibiam <saanmoyolnyibiam@gmail.com>
2024-08-31 14:53:49 +0000

    refactor(models:users): use get_serial for serial num

--- models/users.py
+++ models/users.py
@@ -7,13 +7,16 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from uuid import uuid4
 from datetime import date, datetime
 from flask_login import UserMixin
-from models.base_class import Base, BaseClass
+import models
 
+Base = models.base_class.Base
+BaseClass = models.base_class.BaseClass
+
+format = "%Y-%m-%dT%H:%M:%S.%f"
 
 class User(UserMixin, BaseClass, Base):
-    __count = 0
-
     """Defines a user class"""
+    __count = 0
     __tablename__ = "users"
     serial: Mapped[int] = mapped_column(Integer, nullable=False,
                                         autoincrement=True)
@@ -29,7 +32,7 @@ class User(UserMixin, BaseClass, Base):
     prefix: Mapped[str] = mapped_column(String(10), nullable=True,
                                         default="")
     location: Mapped[str] = mapped_column(String(255), nullable=False)
-    dob: Mapped[date] = mapped_column(Date, nullable=False, default="")
+    dob: Mapped[date] = mapped_column(Date, nullable=False)
     address: Mapped[str] = \
         mapped_column(String(255), nullable=False, default="")
     """
@@ -45,22 +48,24 @@ class User(UserMixin, BaseClass, Base):
     def __init__(self, *args, **kwargs):
         """Initialize user class"""
         if all([kwargs.get("first_name"), kwargs.get("password"),
-                kwargs.get("security_key"), kwargs.get("email")]):
-            to_delete = ["first_name", "security_key", "email",
-                         "username", "password"]
-            self.serial = User.__count + 1
+                kwargs.get("security_key"), kwargs.get("email"),
+                kwargs.get("dob")]):
+            User.__count += 1
+
+            to_delete = ["security_key", "username", "password", "dob"]
+            self.serial = User.get_serial(self, User.__count)
             self.salt = User.generate_salt()
-            self.username = kwargs.get("username") or kwargs["email"]
+            self.username = kwargs.get("username") or f"User-{self.serial}"
             self.passwd_hash = User.generate_hash(text=kwargs["password"],
                                                   salt=self.salt)
             self.security_key = User.generate_hash(text=kwargs["security_key"],
                                                    salt=self.salt)
             self.location = kwargs.get("location") or ""
+            self.dob = date.fromisoformat(kwargs.get("dob"))
             for item in to_delete:
                 if kwargs.get(item):
                     del kwargs[item]
             super().__init__(*args, **kwargs)
-            User.__count += 1
 
     def send_message(self, content, receiver_id, receiver_type="user"):
         """sends a message to a receiver.
