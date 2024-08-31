f3650448b7a42dc4fe3acc3fa43ae02212f2f0ab
William Inyam <sw.inyam@outlook.com>
2024-08-31 00:41:51 +0000

    build(models): add invitations  class

---
+++ models/invitations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""defines the user class"""
+
+
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from models.base_class import Base, BaseClass
+
+
+class Invitation(BaseClass, Base):
+    __count = 0
+
+    """Defines a user class"""
+    __tablename__ = "invitations"
+    serial: Mapped[int] = mapped_column(Integer, nullable=False,
+                                        autoincrement=True)
+    user_from: Mapped[str] = mapped_column(String(32), nullable=False)
+    user_to: Mapped[str] = mapped_column(String(32), nullable=False)
+    link: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """Initialize user class"""
+        if kwargs and all([user_from := kwargs.get('user_from'),
+                           user_to := kwargs.get("user_to"),
+                           link := kwargs.get("link")]):
+            self.user_from = user_from
+            self.user_to = user_to
+            self.link = link
+            super().__init__()
+            Invitation.__count += 1
