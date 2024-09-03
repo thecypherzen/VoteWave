83a569df45b9f800166552e590d29e96e71124bd
Valentine Nyibiam <saanmoyolnyibiam@gmail.com>
2024-09-03 20:58:38 +0000

    build(root): add db setup scripts

--- setup_live.sql
+++ setup_live.sql
@@ -1,7 +1,7 @@
 DROP USER IF EXISTS 'vw_user'@'localhost';
 CREATE USER IF NOT EXISTS
               'vw_user'@'localhost'
-              IDENTIFIED BY 'vw_user#92';
+              IDENTIFIED BY 'vwUser#92';
 CREATE DATABASE IF NOT EXISTS votewave_db;
 GRANT ALL PRIVILEGES ON votewave_db.* TO 'vw_user'@'localhost';
 FLUSH PRIVILEGES;
--- setup_test.sql
+++ setup_test.sql
@@ -1,7 +1,7 @@
 DROP USER IF EXISTS 'vw_test_user'@'localhost';
 CREATE USER IF NOT EXISTS
               'vw_test_user'@'localhost'
-              IDENTIFIED BY 'vw_test_user#92';
+              IDENTIFIED BY 'vwTestUser#92';
 CREATE DATABASE IF NOT EXISTS votewave_test_db;
 GRANT ALL PRIVILEGES ON votewave_test_db.* TO 'vw_test_user'@'localhost';
 FLUSH PRIVILEGES;
