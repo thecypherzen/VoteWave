6fff73c06d9adb9d9b323226afdc7646bae218fe
Valentine Nyibiam <saanmoyolnyibiam@gmail.com>
2024-09-04 06:51:16 +0000

    db(teardown): add script to set up db

--- setup_test.sql
+++ setup_test.sql
@@ -2,7 +2,8 @@ DROP USER IF EXISTS 'vw_test_user'@'localhost';
 CREATE USER IF NOT EXISTS
               'vw_test_user'@'localhost'
               IDENTIFIED BY 'vwTestUser#92';
-CREATE DATABASE IF NOT EXISTS votewave_test_db;
+DROP DATABASE IF EXISTS votewave_test_db;
+CREATE DATABASE votewave_test_db;
 GRANT ALL PRIVILEGES ON votewave_test_db.* TO 'vw_test_user'@'localhost';
 FLUSH PRIVILEGES;
 
