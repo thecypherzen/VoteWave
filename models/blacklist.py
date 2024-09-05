282472b0a9d9f1c78df1f016054d9414f2b17731
Valentine Nyibiam <saanmoyolnyibiam@gmail.com>
2024-09-05 15:43:43 +0000

    feat(models:blacklist): add relationships with polls, elecs, users

--- models/blacklist.py
+++ models/blacklist.py
@@ -1,34 +1,49 @@
 #!/usr/bin/python3
 
-from sqlalchemy import DateTime, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, Integer, \
+    String, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from models.base_class import Base, BaseClass
-
+from typing import List
 
 class Blacklist(BaseClass, Base):
     """Defines a blacklist class"""
     count = 0
-    __tablename__ = "blacklists"
+    __tablename__ = "blacklist"
     serial: Mapped[str] = \
         mapped_column(Integer, nullable=False, autoincrement=True)
-    blocked_user_id: Mapped[str] = mapped_column(String(32), nullable=False)
-    user_id: Mapped[str] = mapped_column(String(32), nullable=True)
-    poll_id: Mapped[str] = mapped_column(String(32), nullable=True)
-    election_id: Mapped[str] = mapped_column(String(32), nullable=True)
+    blocked_user_id: Mapped[str] = mapped_column(
+        ForeignKey("users.id"), nullable=False)
+    user_id: Mapped[str] = mapped_column(
+        ForeignKey("users.id"), nullable=True)
+    poll_id: Mapped[str] = mapped_column(
+        ForeignKey("polls.id"), nullable=True)
+    election_id: Mapped[str] = mapped_column(
+        ForeignKey("elections.id"), nullable=True)
     reason: Mapped[str] = mapped_column(String(128), nullable=False)
-    """
-    polls = relationship()
-    elections = relationship()
-    users = relationship()
-    """
+
+    __table_args__ = (
+        UniqueConstraint("blocked_user_id", "user_id", "poll_id",
+                         "election_id"),
+    )
+
+    # relationships
+    elections: Mapped[List["Election"]] = relationship(
+        back_populates="blacklist_entries",
+        foreign_keys="Blacklist.election_id")
+    polls: Mapped[List["Poll"]] = relationship(
+        back_populates="blacklist_entries",
+        foreign_keys="Blacklist.poll_id")
+    users: Mapped[List["User"]] = relationship(
+        back_populates="blacklist_entries",
+        foreign_keys="Blacklist.user_id")
+
 
     def __init__(self, *args, **kwargs):
-        """Initialises a the candidate class"""
-        if kwargs and all([kwargs.get("reason"),
-                           kwargs.get("blocked_user_id")]):
-            self.blocked_user_id = kwargs.get("blocked_user_id")
-            self.election_id = kwargs.get("election_id")
-            self.user_id = kwargs.get("user_id")
-            self.poll_id = kwargs.get("poll_id")
-            self.reason = kwargs.get("reason")
-            super().__init__()
+        """Initialises a blacklist instance"""
+        if kwargs and \
+           all([kwargs.get("reason"),
+                kwargs.get("blocked_user_id"),
+               any([kwargs.get("user_id"), kwargs.get("poll_id"),
+                    kwargs.get("election_id")])]):
+            super().__init__(*args, **kwargs)
