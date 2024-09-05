aed672a3442a194ac302cfeecd1073202256ead1
Valentine Nyibiam <saanmoyolnyibiam@gmail.com>
2024-09-05 04:26:20 +0000

    feat(models:reviews): add relationship with activities & actors

--- models/reviews.py
+++ models/reviews.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from models.base_class import Base, BaseClass
+from typing import List
 
 
 class Review(BaseClass, Base):
@@ -11,23 +12,32 @@ class Review(BaseClass, Base):
     __tablename__ = "reviews"
     serial: Mapped[str] = \
         mapped_column(Integer, nullable=False, autoincrement=True)
-    user_id: Mapped[str] = mapped_column(String(32), nullable=True)
-    voter_id: Mapped[str] = mapped_column(String(32), nullable=True)
-    candidate_id: Mapped[str] = mapped_column(String(32), nullable=True)
-    poll_id: Mapped[str] = mapped_column(String(32), nullable=True)
-    election_id: Mapped[str] = mapped_column(String(32), nullable=True)
+    user_id: Mapped[str] = \
+        mapped_column(ForeignKey("users.id"), nullable=True)
+    voter_id: Mapped[str] = \
+        mapped_column(ForeignKey("voters.id"), nullable=True)
+    candidate_id: Mapped[str] = \
+        mapped_column(ForeignKey("candidates.id"), nullable=True)
+    poll_id: Mapped[str] = \
+        mapped_column(ForeignKey("polls.id"), nullable=True)
+    election_id: Mapped[str] = \
+        mapped_column(ForeignKey("elections.id"), nullable=True)
     stars: Mapped[str] = mapped_column(Integer, nullable=False)
     message: Mapped[str] = \
         mapped_column(String(255), nullable=False, default="")
-    """
-    polls = relationship()
-    elections = relationship()
-    """
+
+    # relationships
+    poll: Mapped["Poll"] = relationship(back_populates="reviews")
+    election: Mapped["Election"] = relationship(back_populates="reviews")
+    user: Mapped["User"] = relationship(back_populates="reviews")
+    voter: Mapped["Voter"] = relationship(back_populates="reviews")
+    candidate: Mapped["Candidate"] = relationship(back_populates="reviews")
+
 
     def __init__(self, *args, **kwargs):
         """Initialises a the candidate class"""
         if kwargs and kwargs.get("stars") and \
            any([kwargs.get("user_id"), kwargs.get("voter_id"),
                 kwargs.get("candidate_id")]) and \
-                any(kwargs.get("poll_id"), kwargs.get("election_id")):
+                any([kwargs.get("poll_id"), kwargs.get("election_id")]):
             super().__init__(*args, **kwargs)
