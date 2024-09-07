de139f2bbb2151e50dee7b7ccb370231d13e4e78
Valentine Nyibiam <saanmoyolnyibiam@gmail.com>
2024-09-07 01:50:57 +0000

    feat(models:messages): add inbox and metadata relationships

--- models/messages.py
+++ models/messages.py
@@ -2,21 +2,15 @@
 
 from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.dialects.mysql import LONGTEXT
-from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.associationproxy import association_proxy,\
+    AssociationProxy
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from models.base_class import Base, BaseClass
 from typing import List
 
-"""
-message_metadata = Table(
-    "message_metadata", Base.metadata,
-    Column(ForeignKey("messages.id"), primary_key=True,
-           ondelete="CASCADE", onupdate="CASCADE"),
-    Column(ForeignKey("metadata.id"), primary_key=True,
-           ondelete="CASCADE", onupdate="CASCADE")
-)
-"""
+
 class MessageInbox(BaseClass, Base):
+    """defines a message-inbox association table"""
     count = 0
     __tablename__ = "message_inbox"
     serial: Mapped[int] = mapped_column(
@@ -33,6 +27,23 @@ class MessageInbox(BaseClass, Base):
         back_populates="inbox_items")
 
 
+class MessageMetadata(BaseClass, Base):
+    """Defines a message-metadta association"""
+    count = 0
+    __tablename__ = "message_metadata"
+    serial: Mapped[int] = mapped_column(
+        Integer, nullable=False)
+    message_id: Mapped[str] = mapped_column(
+        ForeignKey("messages.id"), primary_key=True)
+    metadata_id: Mapped[str] = mapped_column(
+        ForeignKey("metadata.id"), primary_key=True)
+
+    # relationships
+    message: Mapped["Message"] = relationship(
+        back_populates="mdata_items")
+    mdata: Mapped["Metadata"] = relationship(
+        back_populates="message_items")
+
 
 class Message(BaseClass, Base):
     """Defines a message class"""
@@ -43,13 +54,44 @@ class Message(BaseClass, Base):
     content: Mapped[str] = mapped_column(LONGTEXT, nullable=False)
 
     # relationships
-    inbox_items: Mapped["MessageInbox"] = relationship(
+    inbox_items: Mapped[List[MessageInbox]] = relationship(
         back_populates="message", cascade="all, delete-orphan")
-    inboxes = association_proxy("inbox_items", "inbox")
-    #_metadata: Mapped[List["Metadata"]] = relationship(
-    #   secondary="message_metadata", back_populates="message")
+    mdata_items: Mapped[List[MessageMetadata]] = relationship(
+        back_populates="message", cascade="all, delete-orphan",)
+    inboxes: AssociationProxy[List["Inbox"]] = association_proxy(
+        "inbox_items", "inbox")
+    mdata: AssociationProxy[List["Metadata"]] = association_proxy(
+        "mdata_items", "mdata",
+        creator=lambda data:MessageMetadata(mdata=data))
+
+
 
     def __init__(self, *args, **kwargs):
         """Initialises a message instance"""
         if kwargs and kwargs.get("content"):
             super().__init__(*args, **kwargs)
+
+    def add_metadata(self, *metadata):
+        """Associates given metadata to a message instance"""
+        for meta_item in metadata:
+            if isinstance(metadata, list):
+                for item in meta_item:
+                    self.mdata.append(item)
+            else:
+                self.mdata.append(meta_item)
+        self.save()
+
+    def remove_metadata(self, *metadata):
+        """Dissociates given metadata with message instance"""
+        for meta_item in metadata:
+            if isinstance(meta_item, list):
+                for item in meta_item:
+                    if item in self.mdata:
+                        self.mdata.remove(item)
+            else:
+                if meta_item in self.mdata:
+                    self.mdata.remove(meta_item)
+        self.save()
+
+
+
