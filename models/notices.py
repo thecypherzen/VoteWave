5b25f7bb3b51652b3ba07b4515db4efb2362e474
William Inyam <sw.inyam@outlook.com>
2024-09-10 10:18:26 +0000

    feat(models:notices): update class base defition

--- models/notices.py
+++ models/notices.py
@@ -2,28 +2,29 @@
 
 from datetime import datetime, timedelta
 from sqlalchemy import DateTime, Integer, String
+from sqlalchemy.dialects.mysql import TEXT
 from sqlalchemy.orm import Mapped, mapped_column
 from models.base_class import Base, BaseClass
 
 
 class Notice(BaseClass, Base):
-    """Defines a blacklist class"""
+    """Defines a notice class"""
     count = 0
     __tablename__ = "notices"
-    serial: Mapped[str] = \
-        mapped_column(Integer, nullable=False, autoincrement=True)
-    election_id: Mapped[str] = mapped_column(String(32), nullable=True)
-    poll_id: Mapped[str] = mapped_column(String(32), nullable=True)
-    message: Mapped[str] = mapped_column(String(255), nullable=False)
+    serial: Mapped[int] = mapped_column(
+        Integer, nullable=False, autoincrement=True)
+    owner_id: Mapped[str] = mapped_column(String(32), nullable=True)
+    owner_type: Mapped[str] = mapped_column(String(32), nullable=True)
+    subject: Mapped[str] = mapped_column(String(255), nullable=False)
+    body: Mapped[str] = mapped_column(TEXT, nullable=False)
     expires: Mapped[datetime] = mapped_column(DateTime, nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Initialises a the candidate class"""
-        items = ["election_id", "poll_id"]
-        if kwargs and kwargs.get("message") and \
-           any(key in items for key in kwargs.keys()):
+        """Initialises  of a notice"""
+        items = ["owner_id", "owner_type", "subject", "body"]
+        if kwargs and all(key in items for key in kwargs.keys()):
             self.expires = kwargs.get("expires") or\
-                datetime.now() + timedelta(weeks=10800)
+                datetime.now() + timedelta(days=7)
             if kwargs.get("expires"):
                 del kwargs["expires"]
             super().__init__(*args, **kwargs)
