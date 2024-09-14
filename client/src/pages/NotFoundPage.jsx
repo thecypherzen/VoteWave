24b8b7b09e270d18f466afc44530413a181edc33
William Inyam <sw.inyam@outlook.com>
2024-09-14 14:44:57 +0000

    refactor(client): replace anchor with links

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 export default function NotFound(){
 	return(
 	<>
 	<div>
 		<h1>Sorry, we'd create that later.</h1>
 		<p>For now let's take you back home.</p>
-		<a className="btn btnDark" href="/">Return Home</a>
+		<Link className="btn btnDark" to="/">Return Home</Link>
 	</div>
 	</>
 	);
