c6defcfc934b4ac11d8248af8c62ffb8e7e60c1d
William Inyam <sw.inyam@outlook.com>
2024-09-14 12:27:46 +0000

    build(client): add landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,7 +1,10 @@
+/* Defines the landing page */
+import NavBar from '../components/NavBar';
+
 export default function Landing(){
 	return (
 		<>
-		<div><h1>This is VoteWave Landing Page</h1></div>
+		<NavBar />
 		</>
 	);
 }
