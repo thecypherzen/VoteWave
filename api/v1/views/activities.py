04612264a608489ec375178f9dfe2f230b775014
William Inyam <sw.inyam@outlook.com>
2024-09-15 22:21:20 +0000

    feat(api:views): add activity detail route

--- api/v1/views/activities.py
+++ api/v1/views/activities.py
@@ -4,9 +4,12 @@
 from api.v1.views import app_views
 from flask import json, abort, Response
 from models import storage
+from models.base_activity import Activity
 from models.elections import Election
 from models.polls import Poll
 
+models = {"Activity": Activity, "Election": Election,
+          "Poll": Poll}
 
 @app_views.route("/activities", strict_slashes=False)
 def all_activities():
@@ -29,4 +32,23 @@ def live_activities():
     live = [item for item in data
             if item["status"] == "live"]
     res = json.dumps(live, indent=2) + '\n'
+    return Response(res, mimetype="application/json")
+
+@app_views.route("/activities/<string:activity_id>")
+def activity_detail(activity_id):
+    """Returns the details of an activity by id
+    if the activity is public
+    """
+    print("CALLED.....")
+    session = storage.session()
+    first = session.query(Activity)\
+        .filter_by(id=activity_id).first()
+    if not first:
+        abort(404)
+    type = first.type.capitalize()
+    activity = storage.get(type, activity_id)
+    if not activity:
+        abort(404)
+    print("GOT HERE .....")
+    res = json.dumps(activity.to_dict(), indent=2) + '\n'
     return Response(res, mimetype="application/json")
