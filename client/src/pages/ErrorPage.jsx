0b72bc9d223900a6c734c0b1bee52bb33341b52b
William Inyam <sw.inyam@outlook.com>
2024-09-15 22:24:06 +0000

    refactor(client:pages): update errorpage

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,9 +1,13 @@
-export default function ErrorPage({error, title}){
+import '../styles/errorpage.css';
+
+export default function ErrorPage({error}){
 	return (
-	<div>
-		<h1> Oops! {title}</h1>
-			{console.log(error)}
-		<p></p>
+	<div className="error-content">
+		<h1> Ouch!</h1>
+		<p>{error.message}</p>
+		<p><span>Response Text: </span> {
+			error.response.data.error
+		}</p>
 	</div>
 	);
 }
