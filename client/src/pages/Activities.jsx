7adff21f9a75b22a6d327db731c93dbcba795d35
William Inyam <sw.inyam@outlook.com>
2024-09-15 05:35:01 +0000

    feat(client:activities): add data fetch logic

--- client/src/pages/Activities.jsx
+++ client/src/pages/Activities.jsx
@@ -1,15 +1,35 @@
 /* Defines the Activities Route */
 
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Activities(){
-	const location = useLocation();
-
+	const activities = useLoaderData();
 
 	return (
 		<>
-			<div><h1>Activities Page</h1></div>
+			<section>
+				<h1>Activities</h1>
+				<div id="activities">
+					{activities?.error ?
+					<div>
+						<h3>We got an error</h3>
+						<p><strong>{activities.error.response.statusText}: </strong>{activities.error.message}</p>
+					</div> :
+					<p><b>Status:&nbsp;</b>{activities.data.status}</p>
+					}
+				</div>
+			</section>
 		</>
 	);
+}
+
+export async function loader(){
+	try {
+		const res = await axios.get("http://localhost:8082/api/v1/status");
+		return res;
+	} catch (err) {
+		console.log(err);
+		return {error: err};
+	}
 }
