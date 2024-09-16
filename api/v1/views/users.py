996c7df1817f9747d171d71f2a9dd98489b85077
William Inyam <sw.inyam@outlook.com>
2024-09-16 01:17:13 +0000

    feat(api:views): add more details to user query

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -16,5 +16,20 @@ def user_details(user_id):
     user = session.query(User).filter_by(id=user_id).first()
     if not user:
         abort(404)
-    res = json.dumps(user.to_dict(), indent=2) + '\n'
+
+    # get user's extra information
+    user_dict = user.to_dict()
+
+    own_activities = user.polls + user.elections
+    user_dict["own_activites"] = [act.to_dict() for act in own_activities]
+    user_dict["admin_of"] = [
+        ad.to_dict() for ad in user.admin_of]
+    user_dict["ivs_sent"] = [
+        iv.to_dict() for iv in user.ivs_sent]
+    user_dict["ivs_in"] = [
+        iv.to_dict() for iv in user.ivs_received]
+    user_dict["reviews"] = [rv.to_dict() for rv in user.reviews]
+    user_dict["waitlists"] = [
+        wl.to_dict() for wl in user.waitlists]
+    res = json.dumps(user_dict, indent=2) + '\n'
     return Response(res, mimetype="application/json")
