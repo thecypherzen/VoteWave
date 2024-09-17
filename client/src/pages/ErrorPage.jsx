ad1597aabae0d6cec6d707236a61642ea8ceb3cf
William Inyam <sw.inyam@outlook.com>
2024-09-17 18:33:27 +0000

    fix(client:pages): fix error rendering for error page

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,12 +1,12 @@
 import '../styles/errorpage.css';
 
-export default function ErrorPage({error}){
+export default function ErrorPage(data){
 	return (
 	<div className="error-content">
 		<h1> Ouch!</h1>
-		<p>{error.message}</p>
-		<p><span>Response Text: </span> {
-			console.log(error)
+		<p>{data.error.message}</p>
+		<p><span>code: {data.error.code} </span> {
+			console.log(data)
 		}</p>
 	</div>
 	);
