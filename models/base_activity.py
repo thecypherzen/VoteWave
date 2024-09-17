4beb98b8dfde1cfe9aa3bf66b381bb042be2c897
William Inyam <sw.inyam@outlook.com>
2024-09-17 15:04:23 +0000

    feat(models:base_activity): add status updating method

--- models/base_activity.py
+++ models/base_activity.py
@@ -54,7 +54,6 @@ class Activity(BaseClass, Base):
                 self.ends_at = datetime.strptime(etime, format)
             elif etime.__class__.__name__ == "datetime":
                 self.ends_at = etime
-            self.status = "pending"
             self.salt = self.generate_salt()
             self.security_key = self.generate_hash(
                 text=kwargs.get("security_key"), salt=self.salt)
@@ -68,7 +67,7 @@ class Activity(BaseClass, Base):
                     del kwargs[key]
         super().__init__(*args, **kwargs)
 
-    def set_live_end(self):
+    def update_status(self):
         """Sets the status of an activity model
         to either live or ended depending on its
         start_at and ends_at values
@@ -76,13 +75,20 @@ class Activity(BaseClass, Base):
         starts_diff = self.starts_at - self.created_at
         ends_diff = self.ends_at - self.created_at
         now = datetime.now() - self.created_at
+        changed = False
 
-        print("STATUS BEFORE: ", self.status)
+        if self.status == "pending" \
+            and len(self.candidates):
+            self.status = "active"
+            changed = True
         if now >= starts_diff:
             if now < ends_diff:
-                self.status = "live"
+                if self.status == "active":
+                    self.status = "live"
+                    changed = True
             elif now >= ends_diff:
                 self.status = "ended"
-        print("STATUS AFTER: ", self.status)
-        #self.save()
+                changed = True
+        if changed:
+            self.save()
 
