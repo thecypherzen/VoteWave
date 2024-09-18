b493817cec6f7812e1189c03eaee304919dfaf6e
William Inyam <sw.inyam@outlook.com>
2024-09-18 20:42:37 +0000

    feat(client:components): enable wrapper to accept custom

--- client/src/components/Wrapper.jsx
+++ client/src/components/Wrapper.jsx
@@ -1,6 +1,15 @@
 /* Defines a  container structural component */
 import '../styles/wrapper.css';
 
-export default function Wrapper({children}){
-	return (<div className="wrapper">{children}</div>)
+export default function Wrapper({clist=null, children}){
+	let classes = "wrapper";
+	if (clist)
+	{
+		const listLen = clist.length;
+		for (let i = 0; i < listLen; i++){
+			classes += ` ${clist[i]}`;
+		}
+	}
+	return (
+	<div className={classes}>{children}</div>)
 }
