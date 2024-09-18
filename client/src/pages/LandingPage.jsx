5df016004dc93d19683a9b9f47240ca31de9f841
William Inyam <sw.inyam@outlook.com>
2024-09-18 20:49:06 +0000

    feat(client:pages): add hero section to landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,10 +1,27 @@
 /* Defines the landing page */
 import NavBar from '../components/NavBar';
+import Section, {
+	SectionImage,
+	SectionText
+} from '../components/Section';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 export default function Landing(){
 	return (
 		<>
 		<NavBar />
+		<Section id="hero-section">
+			<SectionText
+				heading="Goodbye to boring voting experiences."
+				body="Now you can vote, play and chat all at once. Join a poll or election to begin."
+				button={{
+					href: "/activities",
+					text: "Join activity",
+					clist: ["btn", "btn-secondary"],
+					icon: faArrowRight
+				}}/>
+			<SectionImage />
+		</Section>
 		</>
 	);
 }
