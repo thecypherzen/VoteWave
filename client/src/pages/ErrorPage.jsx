786218d9fdae4c5745cf0d9ee37800cd0c4d38f8
William Inyam <sw.inyam@outlook.com>
2024-09-21 01:46:27 +0000

    fix(client:errorpage): rectify page break

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,13 +1,25 @@
-import '../styles/errorpage.css';
+import { useNavigate } from 'react-router-dom';
+import Button from '../components/Button';
+import styles from '../styles/errorpage.module.css';
 
 export default function ErrorPage(data){
+	const navigate = useNavigate();
 	return (
-	<div className="error-content">
+	<div className={styles.errorContent}>
 		<h1> Ouch!</h1>
-		<p>{data.error.message}</p>
-		<p><span>code: {data.error.code} </span> {
-			console.log(data)
-		}</p>
+		<p className={styles.errorMessage}>{data?.error?.message ?? "We encountered an error."}</p>
+		{
+			data?.error?.code ?
+			<p><span>code: {data.error.code} </span> {
+				console.log(data)
+			}</p> :
+			""
+		}
+		<div className={styles.btnBox}>
+			<Button clickHandler={()=> {navigate("/")}}
+					clist={["btn", "btn-dark"]}
+					text="Return home"/>
+		</div>
 	</div>
 	);
 }
