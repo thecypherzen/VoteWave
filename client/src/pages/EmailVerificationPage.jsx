43ef00031ad9faabf662466c8c5e507f96f357cd
William Inyam <sw.inyam@outlook.com>
2024-09-23 11:53:40 +0000

    feat(client): make email verfication page prettier

--- client/src/pages/EmailVerificationPage.jsx
+++ client/src/pages/EmailVerificationPage.jsx
@@ -1,8 +1,9 @@
+import styles from '../styles/emailverificationpage.module.css';
 export default function EmailVerificationPage(){
 	return (
-		<>
+		<div className={styles.content}>
 			<h2>One more step</h2>
 			<p>We've sent a verification link to your email. Please click it to complete your login.</p>
-		</>
+		</div>
 	)
 }
